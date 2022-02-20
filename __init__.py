587425b52c635ff3aced681a038b1366bb960bbb
Guy Moulton <75115652+guymoulton-asthorn@users.noreply.github.com>
2022-02-20 10:41:31 +0000

    Add presets

--- __init__.py
+++ __init__.py
@@ -36,33 +36,118 @@ class Radio(CommonPlaySkill):
         self.curl = None
         self.regexes = {}
         self.STREAM = '{}/stream'.format(get_cache_directory('RadioSkill'))
+        self.search_providers = [self.search_default, self.search_presets, self.search_rb]
+        self.rb = RadioBrowser()
 
+    def search_rb(self, search_name):
+        if search_name is None or len(search_name) < 1:
+            return None
+        stations = self.rb.search(name=search_name,bitrate_min=128)
+        if len(stations) > 0:
+            return stations[0]
+        return None
+
+    def search_presets(self, search_name):
+        presets = [
+            {
+                "name"                    : "BBC Radio Cymru",
+                "pronounciation_patterns" : ["camry", "cumry", "bbc radio camry", "radio camry"],
+                "uuid"                    : "84845637-ea22-4d8c-8b2e-dce4b440d9f2"
+            },
+            {
+                "name"                    : "RTÉ Raidió na Gaeltachta",
+                "pronounciation_patterns" : ["rte irish", "irish", "rte radio irish", "irish radio" "radio irish"],
+                "uuid"                    : "bf63c901-5797-4450-8d55-5b8db5835991"
+            },
+            {
+                "name"                    : "RNE Radio Nacional",
+                "pronounciation_patterns" : ["rne radio", "rne radio nacional" "radio rne", "rne"],
+                "uuid"                    : "527c89ae-6e6d-11e9-af37-52543be04c81"
+            },
+            {
+                "name"                    : "Cadena SER - Radio Valencia",
+                "pronounciation_patterns" : ["cadena ser valencia"],
+                "uuid"                    : "bf09733e-0e88-11e9-a80b-52543be04c81"
+            },
+            {
+                "name"                    : "Cadena SER España",
+                "pronounciation_patterns" : ["cadena ser", "cadena", "cadena ser espana"],
+                "uuid"                    : "692a3b69-0f68-11ea-a87e-52543be04c81"
+            }, 
+            {
+                "name"                    : "À Punt Ràdio",
+                "pronounciation_patterns" : ["a punt", "a punt radio"],
+                "uuid"                    : "d53ab822-4756-11e9-aa55-52543be04c81"
+            } 
+        ]
+        self.log.info(f'CPS Match (radio): search_presets search_name is {search_name}')
+        for preset in presets:
+            for pattern in preset["pronounciation_patterns"]:
+                if re.match(pattern, search_name):
+                    self.log.info(f'CPS Match (radio): search_presets found: {preset["name"]}')
+                    station = self.rb.station_by_uuid(preset["uuid"])
+                    if len(station) < 1:
+                        self.log.info(f'search_presets (radio): preset {preset["uid"]} not found in RadioBrowser')
+                        return None
+                    self.log.info(f'CPS Match (radio): search_presets returning station: {type(station)}')
+                    return station[0]
+        self.log.info(f'search_presets (radio): presets returning None')
+        return None
+
+    def search_default(self, search_name):
+        if not (search_name is None or len(search_name) < 1):
+            return None
+        default = {
+            "name" : "BBC Radio Cymru",
+            "pronounciation_matches" : [],
+            "uuid" : "84845637-ea22-4d8c-8b2e-dce4b440d9f2"
+        }
+        station = self.rb.station_by_uuid(default["uuid"])
+        if len(station) < 1:
+            self.log.debug(f'search_default (radio): default radio station not found')
+            return None
+        return station[0]
+        
     def CPS_match_query_phrase(self, phrase):
         # Look for regex matches
         # Play (radio|station|stream) <data>
-        match = re.search(self.translate_regex('radio'), phrase)
-        try:
-            data = re.sub(self.translate_regex('radio'), '', phrase)
-            rb = RadioBrowser()
-            stations = rb.search(name=data,bitrate_min=128)
-            stations != []
-            self.log.info('CPS Match (radio): ' + stations[0]['name'] +
-                          ' | ' + stations[0]['url'])
 
-            if match:
-                return (stations[0]['name'],
-                        CPSMatchLevel.EXACT,
-                        {"station": stations[0]["name"],
-                         "url": stations[0]["url"],
-                         "image": stations[0]['favicon']})
-            else:
-                return (stations[0]['name'],
-                        CPSMatchLevel.TITLE,
-                        {"station": stations[0]["name"],
-                         "url": stations[0]["url"],
-                         "image": stations[0]['favicon']})
-        except Exception:
+        match = re.search(self.translate_regex('radio'), phrase)
+        data = re.sub(self.translate_regex('radio'), '', phrase)
+        station = None
+
+        for provider in self.search_providers:
+            self.log.info(f'CPS Match (radio): trying provider {provider} with search {data}')
+            station = provider(data)
+            self.log.info(f'CPS Match (radio):VV ==================================================')
+            self.log.info(f'CPS Match (radio): provider {provider} found {type(station)}')
+            self.log.info(f'CPS Match (radio): nn==================================================')
+            if station is not None:
+                self.log.info(f'CPS Match (radio): search providers returning type({type(station)})')
+                break
+
+        if station is None:
+            self.log.info('CPS Match (radio): Station not found')
             return None
+        else: 
+            self.log.info(f'CPS Match (radio): Station found {station} - is not none')
+            self.log.info(f'CPS Match (radio): Station found type {type(station)}')
+        
+        if match:
+            self.log.info('CPS Match (radio): ' + station['name'] +
+                        ' | ' + station['url'])
+
+            return (station['name'],
+                    CPSMatchLevel.EXACT,
+                    {"station": station["name"],
+                        "url": station["url"],
+                        "image": station['favicon']})
+        else:
+            return (station['name'],
+                    CPSMatchLevel.TITLE,
+                    {"station": station["name"],
+                        "url": station["url"],
+                        "image": station['favicon']})
 
     def CPS_start(self, phrase, data):
         url = data['url']
